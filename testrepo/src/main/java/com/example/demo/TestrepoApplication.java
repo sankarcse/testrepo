4fe6754822f0400cd98710dcfbc6e954c25ecf08
sthota35 <sthota35@INPF1XMCCP.CSCMWS.CSCMWS.COM>
2020-02-19 17:38:07 +0530

    first commitpopkasd

---
+++ testrepo/src/main/java/com/example/demo/TestrepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestrepoApplication.class, args);
+	}
+
+}
